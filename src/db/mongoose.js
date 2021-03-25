f1cbb0c9f35d06341bac40eae25e3c3d11fdbdbb
Alan <alan.f@msn.com>
2021-03-24 22:24:41 -0300

    First commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+// Database Connection
+const uri = process.env.DB_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully");
+});
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
