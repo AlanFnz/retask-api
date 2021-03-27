5aed14ffd7efc3223040376cb22e33d41c3790a2
Alan <alan.f@msn.com>
2021-03-27 20:48:23 -0300

    Added tasks endpoint. Some refactoring

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
+const express = require('express');
+require('./db');
+const User = require('./models/user');
+const Task = require('./models/task');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body)
+  const user = new User(req.body);
 
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
+  user
+    .save()
+    .then(() => {
+      res.send(user);
     })
-})
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log('Server is up on port ' + port);
+});
