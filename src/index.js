a4709b7c7a2d460892d67e12b01765bd04fd3e6c
Alan <alan.f@msn.com>
2021-03-28 22:08:54 -0300

    Added get users and get user by id endpoints

--- src/index.js
+++ src/index.js
@@ -14,10 +14,36 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((e) => {
       res.status(400).send(e);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
@@ -27,7 +53,7 @@ app.post('/tasks', (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((e) => {
       res.status(400).send(e);
