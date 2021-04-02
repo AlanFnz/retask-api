a58ff451554f79cacc9d2be2ad8366ffbbaf92aa
Alan <alan.f@msn.com>
2021-04-02 17:26:18 -0300

    Hashing passwords before saving and logging users with jwt

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,14 +2,28 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 
-userRouter.post('/users', async (req, res) => {
+router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
@@ -50,10 +64,10 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
@@ -79,4 +93,4 @@ router.delete('/users/:id', async (req, res) => {
   }
 });
 
-module.exports = userRouter;
+module.exports = router;
